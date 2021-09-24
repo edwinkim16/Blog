62cc326c2f0f176fa20e60b305443cf7506191a9
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-24 20:23:24 +0300

    consuming quotes API

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import render_template,redirect,url_for
+from .requests import get_quotes
 from . import main
 
 @main.route('/')
 def index():
+    quote = get_quotes()
    
-    return render_template('index.html')
+    return render_template('index.html',quote=quote)
