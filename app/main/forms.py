265c2bbef4c76f8021f65d8d041365b4c69628a9
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-25 22:19:00 +0300

    blog &comment form and styling changed

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,18 @@ from wtforms.validators import Required,Email
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us something about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+
+
+class BlogForm(FlaskForm):
+
+    title = StringField('Blog title')
+    category= SelectField('Blog Category', choices=[('Select a category', 'Select a category'),('Sports', 'Sports'),('Travel Blog', 'Travel Blog'),('Fitnes Blog', 'Fitness Blog')])
+    content = TextAreaField('Blog category...')
+    submit = SubmitField('Post')
+
+class CommentForm(FlaskForm):
+
+    comment = TextAreaField('Post Of The Comment')
+    submit = SubmitField('Submit')
+
