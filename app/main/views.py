c62edbbf9073f0cf8af196672ed6b99adf30753b
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-25 22:38:07 +0300

    adding new blog and comment route

--- app/main/views.py
+++ app/main/views.py
@@ -2,8 +2,8 @@ from flask import render_template,redirect,url_for,request,abort,flash
 from .requests import get_quotes
 from . import main
 from flask_login import login_required,current_user
-from ..models import User
-from .forms import UpdateProfile
+from ..models import User, Pitch, Comment
+from .forms import UpdateProfile, BlogForm , CommentForm
 from .. import db,photos
 
 @main.route('/')
@@ -49,4 +49,46 @@ def update_pic(uname):
         path = f'photos/{filename}'
         user.profile_pic_path = path
         db.session.commit()
-    return redirect(url_for('main.profile',uname=uname))    
+    return redirect(url_for('main.profile',uname=uname))   
+
+@main.route('/pitch/new', methods=['GET','POST'])
+@login_required
+def new_pitch():
+    form = BlogForm()
+
+    if form.validate_on_submit():
+
+        title=form.title.data
+        content=form.content.data
+        category=form.category.data
+        pitch = Pitch(title=title, content=content,category=category)
+        # pitch.save_pitch(pitch)
+        db.session.add(pitch)
+        db.session.commit()
+
+        flash('Your pitch has been created!', 'success')
+        return redirect(url_for('main.index', id=pitch.id))
+
+    return render_template('new_pitch.html', title='New Post', pitch_form=form, post ='New Post')   
+
+@main.route('/comment/new/<int:id>', methods=['GET','POST'])
+@login_required
+def new_comment(id):
+    form = CommentForm()
+
+    if form.validate_on_submit():
+        
+        comment_content = form.comment.data
+        
+
+        comment = Comment(comment_content= comment_content,pitch_id=id)
+
+        # pitch.save_pitch(pitch)
+        db.session.add(comment)
+        db.session.commit()
+        
+    comment = Comment.query.filter_by(pitch_id=id).all()
+  
+
+
+    return render_template('new_comment.html', title='New Post', comment=comment,comment_form=form, post ='New Post')     
