3a7b87d086c117c7555eb5908e2ea6ae32e2ab76
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-25 20:26:32 +0300

    allow user to see his profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,20 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,request,abort,flash
 from .requests import get_quotes
 from . import main
 from flask_login import login_required
+from ..models import User
 
 @main.route('/')
 def index():
     quote = get_quotes()
    
     return render_template('index.html',quote=quote)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
