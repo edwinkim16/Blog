197128feaf5944b76b9fe2be990380d1f95422f6
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-09-25 11:05:19 +0300

    securing password

--- app/models.py
+++ app/models.py
@@ -1,11 +1,24 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attritube')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
         return f'User {self.username}'
